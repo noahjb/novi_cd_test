3c599d33969dfdc5feb59e78a6b035720f4c6823
Chris Bassano <christo4b@gmail.com>
2016-06-11 15:31:52 -0700

    boilerplate server with comments

--- instructions/branch0.md
+++ instructions/branch0.md
@@ -16,6 +16,18 @@ Here is the folder structure as is:
 
 package.json is used to configure the NPM package manager. We can list dependencies and set up commands in this file. 
 
-If you run the command `npm install`, npm will check this file and install Express and Mongoose.
+- [ ] Run the command `npm install`, npm will check this file and install Express and Mongoose. Your terminal should show a file structure that looks something like this: ![](http://i65.tinypic.com/mif60k.jpg)
 
- 
+## Node/Express Configuration
+
+In our `package.json` file, we indicated that our main file is the `server.js` file. Here is where we will configure the entirety of our back-end. 
+* Connect to Mongo Database
+* Create our Mongoose Models
+* Define routes for our API
+* Define routes for our Angular app
+
+At the top of the file, we're importing the modules that we'll be using to build this application. Then, we have some basic setup to define the port, and define use of middleware.
+
+- [ ] Connect to Mongoose
+- [ ] Set up Mongoose Models
+
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+// Define variables to hold all of the imported modules.
 var express = require('express'); 
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var mongoose = require('mongoose'); // Mongoose is an ORM for MongoDB
+var bodyParser = require('body-parser'); // Middleware that parses JSON
+var morgan = require('morgan'); // Middleware that logs HTTP requests to the console
+var methodOverride = require('method-override'); 
 
-var port = process.env.PORT || 8080;
-var app = express();
+var port = process.env.PORT || 8080; // Defining the port that we're going to be using
+var app = express(); // Creating the application with express
 
-var toDo = mongoose.model('toDo', {
-  text: String,
-  complete: Boolean
-});
+app.use(express.static(__dirname + '/public')); // Sets the location of the static files that we'll be serving
+app.use(morgan('dev')); // Logs every HTTP request to the console
+app.use(bodyParser.json()); // Parses app
+
+app.listen(port); 
+console.log("App listening on port: ", port);
+
+// CONNECT TO MONGO/MONGOOSE
+
+// DEFINE MONGOOSE MODELS
